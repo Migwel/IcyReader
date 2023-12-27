791d165d564369dd951e496cb74d70be1d7ca83d
Migwel <blog@migwel.dev>
2023-12-27 20:34:34 +0100

    Don't run manual test during maven automated runs

--- src/test/java/dev/migwel/icyreader/IcyReaderManualTest.java
+++ src/test/java/dev/migwel/icyreader/IcyReaderManualTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class IcyManualReaderTest {
+public class IcyReaderManualTest {
 
     @Test
     void readNpoRadio1Stream() {
